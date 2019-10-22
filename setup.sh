29bfb4a07cb14607af0685bd8d5584dbf8bb262c
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2019-10-22 13:23:27 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-conda install -n tensorflow_p36 -y seaborn tensorflow-hub ffmpeg
+sudo -u ec2-user -i <<'EOF'
+
+source activate tensorflow_p36
+conda install -y tensorflow-hub seaborn ffmpeg
+source deactivate
+
 sudo yum install -y htop
+
+EOF
