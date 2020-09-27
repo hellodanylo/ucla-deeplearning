eb437276b63dc541ff7976fe375c08d8af93c69a
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2020-09-27 14:55:09 -0700

    uploaded notebooks to s3

--- foundation/jupyter_render.py
+++ foundation/jupyter_render.py
@@ -1,12 +1,17 @@
 import os
 import subprocess
+from typing import Sequence
 
+import boto3
 import nbformat
+import s3fs
+from cachetools import cached, LRUCache
+from nbconvert import HTMLExporter
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbformat import NotebookNode
 
 
-def find_notebooks():
+def find_notebooks() -> Sequence[str]:
     reports = (
         subprocess.run(
             ["find", "/app/ucla_deeplearning", "-name", "*.ipynb"], capture_output=True
@@ -15,16 +20,46 @@ def find_notebooks():
         .split("\n")
     )
 
-    reports = [
-        r
-        for r in reports
-        if "checkpoint" not in r and "Template" not in r and r != ""
-    ]
+    reports = [r for r in reports if "checkpoint" not in r and r != ""]
 
     return reports
 
 
-def render_notebook(path):
+@cached(cache=LRUCache(maxsize=1))
+def get_s3():
+    return boto3.Session(profile_name="ucla").client("s3")
+
+
+def render_notebook(absolute_path: str):
+    with open(absolute_path, "r") as f:
+        notebook_str = f.read()
+
+    s3 = get_s3()
+    ipynb_s3_path = absolute_path[1:]
+    s3.put_object(
+        Bucket="danylo-ucla",
+        ACL="public-read",
+        Key=ipynb_s3_path,
+        Body=notebook_str.encode("utf-8"),
+    )
+    print(f"{absolute_path} -> {ipynb_s3_path}")
+
+    notebook = nbformat.reads(notebook_str, as_version=4)  # type: NotebookNode
+    exporter = HTMLExporter()
+    html = exporter.from_notebook_node(notebook)[0]
+
+    html_s3_path = absolute_path[1:].split(".")[0] + ".html"
+    s3.put_object(
+        Bucket="danylo-ucla",
+        Key=html_s3_path,
+        ACL="public-read",
+        ContentType="text/html",
+        Body=html.encode("utf-8"),
+    )
+    print(f"{absolute_path} -> {html_s3_path}")
+
+
+def execute_notebook(path: str):
     with open(path, "r") as f:
         notebook = nbformat.read(f, as_version=4)  # type: NotebookNode
 
@@ -38,7 +73,6 @@ notebooks = sorted(find_notebooks())
 failed_notebooks = []
 for report_path in notebooks:
     try:
-        print(report_path)
         render_notebook(report_path)
     except Exception as e:
         failed_notebooks.append(report_path)
