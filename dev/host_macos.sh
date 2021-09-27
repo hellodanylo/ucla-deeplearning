9bbfe3a9cffd22b3c48f344dec02d2fb81ca9691
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2021-09-27 12:26:07 -0700

    added conda init zsh

--- dev/host_macos.sh
+++ dev/host_macos.sh
@@ -7,4 +7,5 @@ mkdir opt
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
 bash Miniconda3-latest-MacOSX-x86_64.sh -f -b -p $HOME/opt/miniconda
 
-$HOME/opt/miniconda/bin/conda init
+$HOME/opt/miniconda/bin/conda init bash
+$HOME/opt/miniconda/bin/conda init zsh
