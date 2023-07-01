2b783c589bd46f3aa2255cd0bf72a4dad986f0d5
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2023-07-01 20:53:33 +0000

    use cache from

--- cdk/docker_build.sh
+++ cdk/docker_build.sh
@@ -9,10 +9,10 @@ versioned_uri="${COLLEGIUM_ECR}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $COLLEGIUM_ECR
 
 docker version
-docker build \
-    --cache-from "$latest_uri" \
+docker buildx build \
+    --cache-from="$latest_uri" \
+    --cache-to="type=inline" \
     --build-arg "BASE_IMAGE=${DOCTRINA_ECR}:base-latest" \
     --build-arg "CONDA_ENV=conda_lock.yml" \
-    --build-arg "BUILDKIT_INLINE_CACHE=1" \
     --tag "$versioned_uri" \
     "$project_path"
