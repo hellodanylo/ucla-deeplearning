1c284602b814707d045574202040b9dce246f670
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2024-09-29 18:33:18 +0000

    use ssm to get default env vars

--- cdk/env.sh
+++ cdk/env.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env
+#!/usr/bin/env zsh
 
 project_path=${0:a:h:h}
 
 conda activate collegium 2>/dev/null || true
 
 set -a;
-source $project_path/cdk/.env
+source $project_path/cdk/.env || true
 set +a;
 
-export CODEBUILD_RESOLVED_SOURCE_VERSION=$(git rev-parse HEAD)
+app_vars=$(aws --region $AWS_REGION ssm get-parameter --name /collegium/app-resources | jq -r .Parameter.Value)
+build_vars=$(aws --region $AWS_REGION ssm get-parameter --name /collegium/build-resources | jq -r .Parameter.Value)
+
+export COLLEGIUM_ECR=$(echo $build_vars | jq -r .collegium_ecr)
+export DOCTRINA_ECR=$(echo $app_vars | jq -r .doctrina_ecr)
+export CODEBUILD_RESOLVED_SOURCE_VERSION=${CODEBUILD_RESOLVED_SOURCE_VERSION:-$(git rev-parse HEAD)}
