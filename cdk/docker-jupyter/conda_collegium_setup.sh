fa4b2b0daacd0ac4f2fa925d3e47ed2d729c6405
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2024-10-06 20:55:13 +0000

    update cdk cli

--- cdk/docker-jupyter/conda_collegium_setup.sh
+++ cdk/docker-jupyter/conda_collegium_setup.sh
@@ -5,7 +5,7 @@ conda_path=${0:a:h}
 
 conda env create -n collegium -f $conda_path/conda_collegium.yml
 # /app/conda/envs/collegium/bin/python -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
-conda run -n collegium npm -g install aws-cdk@2.156.0
+conda run -n collegium npm -g install aws-cdk@2.161.0
 
 /app/conda/envs/collegium/bin/ipython kernel install --user --name=collegium
 
