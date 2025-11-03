9e9bc8cf2880f7c060e1e5c6c81d136bef8faf50
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2025-11-03 18:05:51 +0000

    update sagemaker jupyter config

---
+++ cdk/docker-jupyter/install_go.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+cd /app
+wget https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
+tar xvf go1.25.3.linux-amd64.tar.gz
+rm go1.25.3.linux-amd64.tar.gz
--- cdk/docker-jupyter/jupyter_server_config.py
+++ cdk/docker-jupyter/jupyter_server_config.py
@@ -1,10 +1,19 @@
+from amazon_sagemaker_jupyter_scheduler.scheduler import SageMakerStudioLabScheduler
+from amazon_sagemaker_jupyter_scheduler.environments import SagemakerEnvironmentManager
+from amazon_sagemaker_jupyter_scheduler.file_download_manager import SageMakerJobFilesManager
+
+c.SchedulerApp.scheduler_class = SageMakerStudioLabScheduler
+c.SchedulerApp.environment_manager_class = SagemakerEnvironmentManager
+c.SchedulerApp.job_files_manager_class = SageMakerJobFilesManager
+
 c.ServerProxy.servers = {
    "mlflow": {
-       "command": ["/app/conda/envs/collegium/bin/python", "-m", "mlflow", "server", "--port", "{port}", "--backend-store-uri", "file:///app/mlflow"],
+        "command": ["/opt/conda/bin/python", "-m", "mlflow", "server", "--port", "{port}", "--backend-store-uri", "file:///home/sagemaker-user/mlflow"],
+        "timeout": 60,
         'launcher_entry': {
             'enabled': True,
             'title': 'mlflow',
-            'icon_path': '/app/conda/envs/jupyter/mlflow.svg'
+            'icon_path': '/app/collegium/cdk/docker-jupyter/mlflow.svg'
         }
    }
 }
