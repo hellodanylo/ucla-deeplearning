3f03cf5ded5b49e3077d76cfc630079540d0ca0a
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2025-11-09 17:17:10 +0000

    install keras hub

--- cdk/docker-jupyter/install_go.sh
+++ cdk/docker-jupyter/install_go.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
+set -eux
+
 cd /app
-wget https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
-tar xvf go1.25.3.linux-amd64.tar.gz
+wget -q https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
+tar xf go1.25.3.linux-amd64.tar.gz
 rm go1.25.3.linux-amd64.tar.gz
